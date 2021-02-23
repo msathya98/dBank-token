ff752579f1d3897d490211d09b590995bd8e242a
Sathya M <62427113+msathya98@users.noreply.github.com>
2021-02-23 19:45:37 +0530

    Token.sol for creating  tokens by _mint( ) function.

---
+++ contracts/Token.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+  //add minter variable
+ address public minter;
+
+event MinterChanged(address indexed from, address to);
+
+
+  constructor() payable ERC20("Decentralized Bank Currency", "DCB") {
+    minter = msg.sender;
+    //asign initial minter
+  }
+
+  function  passMinterRole(address dBank) public returns (bool) {
+    require(msg.sender == minter, "Error, only owner can change pass minter role");
+    minter = dBank;
+
+    emit MinterChanged(msg.sender, dBank);
+    return true;
+    
+  }
+
+  function mint(address account, uint256 amount) public {
+    require(msg.sender == minter, "Error, msg.sender does not have minter role");
+		_mint(account, amount);
+	}
+}
